0e99820fe913609d81baa3278feb34ace42b55f4
shain-labs <211713959+shain-labs@users.noreply.github.com>
2025-09-04 14:14:46 +0000

    chore: finalise instruction module exports

--- programs/shain/src/instructions.rs
+++ programs/shain/src/instructions.rs
@@ -1,5 +1,9 @@
+pub mod close_session;
+pub mod gated_action;
 pub mod initialize;
 pub mod start_session;
 
+pub use close_session::*;
+pub use gated_action::*;
 pub use initialize::*;
 pub use start_session::*;
